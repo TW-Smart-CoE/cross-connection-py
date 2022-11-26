49403fd2a32e92b12e34f7f02479d57f9f284610
Jie Meng <jmengxy@gmail.com>
2022-11-26 10:17:31 +0800

    Fix code convention of PropKeys

--- cconn/definitions/prop_keys.py
+++ cconn/definitions/prop_keys.py
@@ -7,8 +7,8 @@ class PropKeys:
     PROP_IP: Final = 'ip'
     PROP_PORT: Final = 'port'
     PROP_AUTO_CONNECT: Final = 'auto_reconnect'
-    PROP_MIN_RECONNECT_RETRY_TIME: Final = "min_reconnect_retry_time"
-    PROP_MAX_RECONNECT_RETRY_TIME: Final = "max_reconnect_retry_time"
+    PROP_MIN_RECONNECT_RETRY_TIME: Final = 'min_reconnect_retry_time'
+    PROP_MAX_RECONNECT_RETRY_TIME: Final = 'max_reconnect_retry_time'
 
     PROP_UDP_DETECTOR_BROADCAST_PORT: Final = 'broadcast_port'
     PROP_UDP_DETECTOR_FLAG = 'flag'
